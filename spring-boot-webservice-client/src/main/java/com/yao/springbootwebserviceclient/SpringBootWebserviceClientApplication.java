81a1540cf923c10b0670a488f65b6efd8bc96b9d
yaoyao <15656009880@163.com>
2019-10-24 09:45:15 +0800

    新建测试项目

---
+++ spring-boot-webservice-client/src/main/java/com/yao/springbootwebserviceclient/SpringBootWebserviceClientApplication.java
@@ -0,0 +1,13 @@
+package com.yao.springbootwebserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebserviceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebserviceClientApplication.class, args);
+    }
+
+}
