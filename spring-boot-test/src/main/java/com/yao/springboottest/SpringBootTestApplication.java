b93d70575fd7dc729495c74fb49038d555c0be38
yaoyao <15656009880@163.com>
2020-03-11 15:57:30 +0800

    解析schema文件

--- spring-boot-test/src/main/java/com/yao/springboottest/SpringBootTestApplication.java
+++ spring-boot-test/src/main/java/com/yao/springboottest/SpringBootTestApplication.java
@@ -3,6 +3,9 @@ package com.yao.springboottest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * qidong1
+ */
 @SpringBootApplication
 public class SpringBootTestApplication {
 
